f1687a911e50a95b19898dc1cf1c7a642daa557b
Jessica <jessica.schwabegger@hotmail.com>
2019-11-08 12:44:04 +0100

    in progress

--- src/scripts/order.js
+++ src/scripts/order.js
@@ -1,32 +1,135 @@
-// export {placeOrder};
+export {placeOrder};
 
-// function placeOrder() {
+function placeOrder() {
 
-//     let orderBtn = document.querySelector(".rdy-btn");
-//     let roomBox = document.querySelector(".room-section");
-//     let styleBox = document.querySelector(".style-section");
-//     let closeBtn = document.querySelectorAll(".close-btn");
+    let orderBtn = document.querySelector(".rdy-btn");
+    let roomBox = document.querySelector(".room-section");
 
-//     orderBtn.addEventListener("click", function(){
+    orderBtn.addEventListener("click", function(){
 
-//         orderBtn.classList.add("hide");
-//         roomBox.classList.remove("hide");
+        
+        roomBox.classList.remove("hide");
+        chooseRoom();
 
-//     });
+    });
 
-//     closeBtn.addEventListener("click", function(){
+}
 
-//         styleBox.classList.add("hide");
-//         orderBtn.classList.remove("hide");
+function confirmOrder(myArr) {
+    let i = 0;
+    let roomObjects = document.querySelectorAll(".final-section p");
+    for(let object of roomObjects) {
+        console.log(roomObjects);
 
-//     });
+        if (i >= myArr.length){
+            
+        }
+        
+        if (i <= myArr.length -1) {
+            object.innerText = object.innerHTML + myArr[i];
+            i++;
+        }else {
+            object.innerText = object.innerHTML + "hej";
+        }
+    }
+
+}
+
+function chooseRoom() {
+
+    let roomBox = document.querySelector(".room-section");
+    let styleBox = document.querySelector(".style-section");
+    let rooms = document.querySelectorAll(".room-section img");
+    let styles = document.querySelectorAll(".style-section img");
+    let closeBtns = document.querySelectorAll(".close-btn");
+    let areaBox = document.querySelector(".area-section");
+    let input = document.querySelector("input");
+    let finalBox = document.querySelector(".final-section");
+    let myArr = [];
+
+    for (let room of rooms) {
+        room.addEventListener("click", event => {
+            let myRoom = event.target.classList.value;
+
+            //myObject(myRoom);
+            if (myArr.length == 1) {
+                // myArr.pop();
+                // myArr.push(myRoom);
+                roomBox.classList.add("hide");
+                styleBox.classList.remove("hide");
+            }else {
+                myArr.push(myRoom);
+                roomBox.classList.add("hide");
+                styleBox.classList.remove("hide");
+            }
+        });
+    }
+
+    for (let style of styles) {
+        style.addEventListener("click", event => {
+            let myStyle = event.target.classList.value;
+            if (myArr.length == 2){
+                // myArr.pop();
+                // myArr.push(myStyle);
+                styleBox.classList.add("hide");
+                areaBox.classList.remove("hide");
+            }else {
+                myArr.push(myStyle);
+                styleBox.classList.add("hide");
+                areaBox.classList.remove("hide");
+            }
+        });
+    }
+
+    input.addEventListener("change", function(){
+
+            let myArea = input.value;
+            myArr.push(myArea);
+            //myObject(myArr);
+            areaBox.classList.add("hide");
+            finalBox.classList.remove("hide");
+
+            confirmOrder(myArr);
+
+    });
+
+    for (let button of closeBtns) {
+        button.addEventListener("click", event =>{
+
+                roomBox.classList.add("hide");
+                styleBox.classList.add("hide");
+                areaBox.classList.add("hide");
+                finalBox.classList.add("hide");
+
+        });
+    }
+}
 
 
+// Behövdes ej, ville mest bara göra ett objekt :)))))
+// function myObject(myArr) {
+
+//     let myChoice = {
+
+//         Room: myArr[0],
+//         style: myArr[1],
+//         m2: myArr[2]
+//     };
+//     console.log(myChoice);
 // }
 
-// function closeButton() {
 
-    
-// }
+let priceOffer = {
+
+    Rooms: {
+        Bedroom: 800,
+        Kitchen: 700,
+        Bathroom: 500,
+        Living_room: 1000,
+        Kids_room: 800,
+        Entrance: 500
+    }
+
+};
 
 
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,76 +1,76 @@
-// export {imgSlide};
-// let index = 0;
-// function nextBtn(index, list) {
+export {imgSlide};
+let index = 0;
+function nextBtn(index, list) {
     
-//     if (index >= list.length -1) {
+    if (index >= list.length -1) {
 
-//         index = 0 ;
-//         list[index].classList.remove("hide");
-//         list[index+1].classList.add("hide");
+        index = 0 ;
+        list[index].classList.remove("hide");
+        list[index+1].classList.add("hide");
 
 
-//     }else {
-//         list[index].classList.add("hide");
-//         list[index+1].classList.remove("hide");
-//     }
+    }else {
+        list[index].classList.add("hide");
+        list[index+1].classList.remove("hide");
+    }
 
     
-//     console.log(index);
-//     console.log(list[index]);
+    console.log(index);
+    console.log(list[index]);
 
-// }
+}
 
-// function previousBtn(index, list) {
+function previousBtn(index, list) {
 
   
-//     if (index <= 0) {
-//         index = list.length-1;
+    if (index <= 0) {
+        index = list.length-1;
 
-//         list[index].classList.remove("hide");
-//         list[0].classList.add("hide");
+        list[index].classList.remove("hide");
+        list[0].classList.add("hide");
 
-//     }else {
-//         list[index].classList.add("hide");
-//         list[index-1].classList.remove("hide");
+    }else {
+        list[index].classList.add("hide");
+        list[index-1].classList.remove("hide");
 
-//     }
-//     console.log(index);
+    }
+    console.log(index);
 
-//     console.log(list[index]);
-
-
-// }
-
-// function newImg(){
+    console.log(list[index]);
 
 
-// }
+}
 
-// function imgSlide() {
+function newImg(){
+
+
+}
+
+function imgSlide() {
 
     
-//     console.log(index);
-//     let leftBtn = document.querySelector(".left-btn");
-//     let rightBtn = document.querySelector(".right-btn");
-//     let images = document.querySelectorAll(".pictures");
+    console.log(index);
+    let leftBtn = document.querySelector(".left-btn");
+    let rightBtn = document.querySelector(".right-btn");
+    let images = document.querySelectorAll(".pictures");
     
-//     rightBtn.addEventListener("click", function(){
+    rightBtn.addEventListener("click", function(){
 
-//         index++;
-//         nextBtn(index, images);
+        index++;
+        nextBtn(index, images);
         
 
-//      });
+     });
 
-//      leftBtn.addEventListener("click", function(){
+     leftBtn.addEventListener("click", function(){
 
-//         index--;
-//         previousBtn(index, images);
+        index--;
+        previousBtn(index, images);
     
 
-//      });
+     });
 
      
-// }
+}
 
 
