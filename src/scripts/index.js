42d4069d4629075e020c240f4e9cacec2da8048e
Jessica <jessica.schwabegger@hotmail.com>
2019-11-12 12:41:01 +0100

    In progress

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,13 @@
 import '../styles/index.scss';
-//import anime from 'animejs';
+// import anime from 'animejs/lib/anime.es.js';
 import {imgSlide} from './slider';
 import {placeOrder} from './order';
 
 
+
 if(window.location.pathname == "/order.html") {
     placeOrder();
+
 }else if(window.location.pathname == "/index.html") {
     imgSlide();
 }
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,16 +1,29 @@
 export {imgSlide};
+import anime from 'animejs';
+
+function fadeIn(target) {
+
+    anime({
+      targets: target,
+      opacity: [0, 1],
+      easing: "linear",
+      duration: 2000
+
+    });
+}
 
 function imgSlide() {
 
+
     let index = 0;
-    console.log(index);
     let leftBtn = document.querySelector(".left-btn");
     let rightBtn = document.querySelector(".right-btn");
     let images = document.querySelectorAll(".pictures");
+    fadeIn(images);
     
     rightBtn.addEventListener("click", function(){
-
         index++;
+        fadeIn(images);
 
         if (index > images.length -1) {
 
@@ -22,35 +35,31 @@ function imgSlide() {
             
             images[index].classList.remove("hide");
             images[index-1].classList.add("hide");
-    
         }
-        console.log(index);
-        
 
      });
 
      leftBtn.addEventListener("click", function(){
-
         index--;
+        fadeIn(images);
+
         if (index < 0) {
+
             index = images.length -1;
-    
             images[index].classList.remove("hide");
             images[0].classList.add("hide");
             
-    
         }else {
+            
             images[index].classList.remove("hide");
             images[index+1].classList.add("hide");
-    
         }
-    
-        console.log(index);
-    
-
      });
 
-     
 }
+
+
+
+
 
 
